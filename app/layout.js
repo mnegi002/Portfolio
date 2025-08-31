e9a668c5b303ce362173677db37f3073bd6e53ba
Mayank Negi <mayank@Mayanks-MacBook-Air.local>
2025-08-31 18:04:59 +0530

    meta data

--- app/layout.js
+++ app/layout.js
@@ -5,22 +5,25 @@ import Footer from "@/components/elements/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// 🔑 Add this
+export const metadata = {
+  title: "CoderRoom - By Mayank&Co.",
+  description: "Turning ideas into Reality",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <main className="flex ">
+        <main className="flex">
           <div className="side flex-none">
             <SidebarDemo />
           </div>
           <div className="child flex-1 overflow-x-hidden">
             {children}
           </div>
-          
         </main>
-        <div className="">
-            <Footer/>
-          </div>
+        <Footer />
       </body>
     </html>
   );
